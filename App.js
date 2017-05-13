cf3051964a8c118208576da7946ac06ffc6aa14a
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-05-13 16:23:51 +0700

    hot reloader has problems with git commit

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 import HoiBrowser from './HoiBrowser'
 
+import RecordCreateForm from './RecordCreateForm'
+
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <HoiBrowser />
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={{flex: 1}}>
+				<View style={styles.container}>
+					<Text>What are you doing?</Text>
+				</View>
+				<HoiBrowser style={{flex: 1}}/>
+				<RecordCreateForm style={{flex: 1}}/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    /* alignItems: 'center', */
-    /* justifyContent: 'center', */
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
+/////
---
+++ RecordCreateForm.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
+
+export default class RecordCreateForm extends React.Component {
+	render() {
+
+
+
+
+		return (
+			<View style={styles.container}>
+				<View id="step1">
+					<Text>What are you doing?</Text>
+					<TextInput
+						style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+						onChangeText={(text) => this.setState({text})}
+						value={this.state.text}
+					/>
+				</View>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
